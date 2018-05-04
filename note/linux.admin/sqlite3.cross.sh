2d674d68ec9ff4d9b6bc1cb8d9f0b1dc1a28aa85
s_baoshan <s_baoshan@163.com>
2018-05-04 08:06:26 +0800

    misc

---
+++ note/linux.admin/sqlite3.cross.sh
@@ -0,0 +1,4 @@
+./configure --prefix=/home/lhl/soft/sqlite3/sqlite-autoconf-3230100/install --host=arm-linux CC=/home/lhl/soft/arm-2014.05/bin/arm-none-linux-gnueabi-gcc
+
+make
+make install
---
+++ note/misc/Linux如何查看JDK的安装路径_Linux教程_Linux公社-Linux系统门户网站_files/linux.js
@@ -0,0 +1,1 @@
+document.writeln("<a href=\'http://www.enkj.com/idc/zuyong/\' target=\'_blank\'><img src=\'/linuxfile/ye.gif\'  width=\'600\' height=\'65\'></a>");
